fdcbd7d1fab372cf33e528d98c9c7f9e3b6b1a2a
SixLeopard <43029873+SixLeopard@users.noreply.github.com>
2020-02-20 10:35:34 +1000

    put everything in one window
    put everything in one window as well as some other small chnages

--- Draw on tap.py
+++ Draw on tap.py
@@ -5,14 +5,20 @@ import random
 import sqlite3
 print("copyright Jamie Westerhout, 2019")
 root = tk.Tk()
-root.geometry('{}x{}'.format(10, 500))
+root.configure(bg="darkgrey")
 frame = tk.Frame(root)
+canvas = tk.Canvas(master = root, width = 900, height = 900, bg="black")
+canvas.pack()
+screen = turtle.TurtleScreen(canvas)
+t = turtle.RawTurtle(screen)
+t.shape("circle")
+t.shapesize(0.1)
+screen.bgcolor("white")
 frame.pack()
-turtle.bgcolor("black")
-screen = turtle.Screen()
+#screen = t.TurtleScreen()
 loop2 = 1
 test = 1
-turtle.color("red")
+t.color("red")
 def pen1():
     screen.onscreenclick(draw1)
 def pen2():
@@ -20,158 +26,159 @@ def pen2():
 def pen3():
     screen.onscreenclick(draw3)
 def pen4():
-    turtle.penup()
+    t.penup()
     screen.onscreenclick(draw4)
 def pen5():
-    turtle.penup()
+    t.penup()
     screen.onscreenclick(none)
 def pen7():
-    turtle.penup()
+    t.penup()
     screen.onscreenclick(Record)
 def none(x, y):
-    turtle.penup()
-    turtle.goto(x, y)
+    t.penup()
+    t.goto(x, y)
     print("nothing")
 def BGBlack():
-    turtle.bgcolor("black")
+    screen.bgcolor("black")
 def BGWhite():
-    turtle.bgcolor("white")
+    screen.bgcolor("white")
 def PRed():
-    turtle.pencolor("Red")
+    screen.pencolor("Red")
 def PWhite():
-    turtle.pencolor("white")
+    t.pencolor("white")
 def PBlack():
-    turtle.pencolor("black")
+    t.pencolor("black")
 def PBlue():
-    turtle.pencolor("Blue")
+    t.pencolor("Blue")
 def Record(x, y):
-    turtle.goto(x, y)
-    turtle.pendown()
+    t.goto(x, y)
+    t.pendown()
     f = open('CustomPen.pen','a+')
-    f.write(str(float(turtle.xcor())))
+    f.write(str(float(t.xcor())))
     f.write(", ")
-    f.write(str(float(turtle.ycor())))
+    f.write(str(float(t.ycor())))
     f.write("\n")
     f.close()
-    print(str(turtle.pos()))
+    print(str(t.pos()))
 def CustomPen1():
     p = open('CustomPen.pen','r')
-    turtle.penup()
+    t.penup()
     for x in p:
         x1, y1 = map(str , x.split())
         x1 = x1[:-1]
-        turtle.goto(float(x1), float(y1))
-        turtle.pendown()
+        t.goto(float(x1), float(y1))
+        t.pendown()
     p.close
 def draw1(x , y):
-    turtle.penup()
-    turtle.goto(x, y)
+    t.penup()
+    t.goto(x, y)
     Bac()
 def draw2(x , y):
-    turtle.penup()
-    turtle.goto(x, y)
+    t.penup()
+    t.goto(x, y)
     star()
 def draw3(x , y):
-    turtle.penup()
-    turtle.goto(x, y)
+    t.penup()
+    t.goto(x, y)
     other()
 def draw4(x , y):
-    turtle.goto(x, y)
-    turtle.pendown()
+    t.goto(x, y)
+    t.pendown()
 def Bac():
-    turtle.setheading(90)
-    turtle.penup()
-    turtle.forward(90)
-    turtle.setheading(180)
-    turtle.pendown()
+    t.setheading(90)
+    t.penup()
+    t.forward(90)
+    t.setheading(180)
+    t.pendown()
     loop = 1
     legnth = 1
     speed = 10000
     angle = 1
     while loop <= 200:
-        turtle.forward(legnth)
-        turtle.left(angle)
-        turtle.speed(10000000)
+        t.forward(legnth)
+        t.left(angle)
+        t.speed(10000000)
         legnth += 1.5
         speed += 1.5
         angle += 1
         loop += 1
-        turtlexcord = turtle.xcor()
-        turtleycord = turtle.ycor()
-        print(turtlexcord , turtleycord)
-        #if turtlexcord >= 250:
-        #    turtle.color("green")
-        #    if turtleycord >= 250:
-        #      turtle.color("red")
-        #elif turtlexcord <= 250:
-        #    turtle.color("blue")
-        #    if turtleycord <= 250:
-        #        turtle.color("black")
+        txcord = t.xcor()
+        tycord = t.ycor()
+        print(txcord , tycord)
+        #if txcord >= 250:
+        #    t.color("green")
+        #    if tycord >= 250:
+        #      t.color("red")
+        #elif txcord <= 250:
+        #    t.color("blue")
+        #    if tycord <= 250:
+        #        t.color("black")
 def star():
-    turtle.penup()
-    turtle.setheading(180)
-    turtle.pendown()
+    t.penup()
+    t.setheading(180)
+    t.pendown()
     loop = 1
     legnth = 100
     speed = 10000
     angle = 200
     while loop <= 18:
-        turtle.forward(legnth)
-        turtle.left(angle)
-        turtle.speed(10000000)
+        t.forward(legnth)
+        t.left(angle)
+        t.speed(10000000)
         legnth += 1.5
         speed += 1.5
         angle += 1
         loop += 1
-        turtlexcord = turtle.xcor()
-        turtleycord = turtle.ycor()
-        print(turtlexcord , turtleycord)
+        txcord = t.xcor()
+        tycord = t.ycor()
+        print(txcord , tycord)
 def other():
-    turtle.penup()
-    turtle.setheading(180)
-    turtle.pendown()
+    t.penup()
+    t.setheading(180)
+    t.pendown()
     loop = 1
     legnth = 50
     speed = 10000
     angle = 90
     while loop <= 50:
-        turtle.forward(legnth)
-        turtle.left(angle)
-        turtle.speed(10000000)
+        t.forward(legnth)
+        t.left(angle)
+        t.speed(10000000)
         legnth += 1.5
         speed += 1.5
         angle += 1
         loop += 1
-        turtlexcord = turtle.xcor()
-        turtleycord = turtle.ycor()
-        print(turtlexcord , turtleycord)
-button = tk.Button(frame, text="QUIT", fg="red", command=quit)
-button.pack(side=tk.BOTTOM, padx=[10,20])
-button = tk.Button(frame, text="Shape 1", fg="black", command=pen1)
-button.pack(side=tk.BOTTOM, padx=[10,20])
-button = tk.Button(frame, text="Shape 2", fg="black", command=pen2)
-button.pack(side=tk.BOTTOM, padx=[10,20])
-button = tk.Button(frame, text="Shape 3", fg="black", command=pen3)
-button.pack(side=tk.BOTTOM, padx=[10,20])
-button = tk.Button(frame, text="Pen", fg="black", command=pen4)
-button.pack(side=tk.BOTTOM, padx=[10,20])
-button = tk.Button(frame, text="none", fg="black",command=pen5)
-button.pack(side=tk.BOTTOM, padx=[10,20])
-button = tk.Button(frame, text="Play", fg="black",command=CustomPen1)
-button.pack(side=tk.BOTTOM, padx=[10,20])
-button = tk.Button(frame, text="Record", fg="black",command=pen7)
-button.pack(side=tk.BOTTOM, padx=[10,20])
-button = tk.Button(frame, text="BG White", fg="black",command=BGWhite)
-button.pack(side=tk.BOTTOM, padx=[20,20])
-button = tk.Button(frame, text="BG Black", fg="black",command=BGBlack)
-button.pack(side=tk.BOTTOM, padx=[20,20])
-button = tk.Button(frame, text="Pen red", fg="black",command=PRed)
-button.pack(side=tk.BOTTOM, padx=[20,20])
-button = tk.Button(frame, text="Pen Black", fg="black",command=PBlack)
-button.pack(side=tk.BOTTOM, padx=[20,20])
-button = tk.Button(frame, text="Pen White", fg="black",command=PWhite)
-button.pack(side=tk.BOTTOM, padx=[20,20])
-button = tk.Button(frame, text="Pen Blue", fg="black",command=PBlue)
+        txcord = t.xcor()
+        tycord = t.ycor()
+        print(txcord , tycord)
+button = tk.Button(master = root, text="QUIT", fg="red", command=quit, highlightthickness=5, bd=0.5)
+button.pack(side=tk.LEFT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Shape 1", fg="black", command=pen1, highlightthickness=5, bd=0.5)
+button.pack(side=tk.LEFT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Shape 2", fg="black", command=pen2, highlightthickness=5, bd=0.5)
+button.pack(side=tk.LEFT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Shape 3", fg="black", command=pen3, highlightthickness=5, bd=0.5)
+button.pack(side=tk.LEFT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Pen", fg="black", command=pen4, highlightthickness=5, bd=0.5)
+button.pack(side=tk.LEFT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="none", fg="black",command=pen5, highlightthickness=5, bd=0.5)
+button.pack(side=tk.LEFT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Play", fg="black",command=CustomPen1, highlightthickness=5, bd=0.5)
+button.pack(side=tk.LEFT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Record", fg="black",command=pen7, highlightthickness=5, bd=0.5)
+button.pack(side=tk.RIGHT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="BG White", fg="black",command=BGWhite, highlightthickness=5, bd=0.5)
+button.pack(side=tk.RIGHT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="BG Black", fg="black",command=BGBlack, highlightthickness=5, bd=0.5)
+button.pack(side=tk.RIGHT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Pen red", fg="black",command=PRed, highlightthickness=5, bd=0.5)
+button.pack(side=tk.RIGHT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Pen Black", fg="black",command=PBlack, highlightthickness=5, bd=0.5)
+button.pack(side=tk.RIGHT, padx=[10,20], pady=[5,5])
+button = tk.Button(master = root, text="Pen White", fg="black",command=PWhite, highlightthickness=5, bd=0.5)
+button.pack(side=tk.RIGHT, padx=[10,20], pady=[0,0])
+button = tk.Button(master = root, text="Pen Blue", fg="black",command=PBlue, highlightthickness=5, bd=0.5)
+button.pack(side=tk.RIGHT, padx=[10,20], pady=[0,0])
 button.pack()
 screen.listen()
 screen.mainloop()
